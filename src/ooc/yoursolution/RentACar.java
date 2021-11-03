7feb736ca5e38b271eadebd948b096d42c3f8d91
SeanMelfrancoUy <72450766+SeanUy2001@users.noreply.github.com>
2021-11-03 16:55:37 +0000

    Creating Car and RentACar class
    Creating two new class

---
+++ src/ooc/yoursolution/RentACar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ooc.yoursolution;
+
+/**
+ *
+ * @author seanuy
+ */
+public class RentACar implements RentACarInterface {
+
+    public RentACar() {
+    }
+    
+}
