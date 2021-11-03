550b34b589f0d63f16e9595ee0fc4aeb6ea5bf79
SeanMelfrancoUy <72450766+SeanUy2001@users.noreply.github.com>
2021-11-03 16:57:38 +0000

    Adding BufferedReader

--- src/ooc/yoursolution/BookingSystem.java
+++ src/ooc/yoursolution/BookingSystem.java
@@ -5,10 +5,36 @@
  */
 package ooc.yoursolution;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  *
  * @author seanuy
  */
-public class BookingSystem {
+public class BookingSystem implements BookingSystemInterface {
+
+    @Override
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+         
+           RentACarInterface RentACar = new RentACar();
+           
+           String NumberOfCars = in.readLine();
+           String Make = "";
+           
+           while(NumberOfCars != null) {
+               
+               Make = in.readLine();
+               
+               Car cars = new Car();
+               RentACar.getNumberOfCars();
+               RentACar.getName();
+               
+               NumberOfCars = in.readLine();
+               
+           }
+           
+           
     
-}
+    
+
